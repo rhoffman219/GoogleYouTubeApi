570943b01d6ecda9e469c0116177a104ca6573c6
Richard Hoffman <rhoffman219@gmail.com>
2018-01-24 21:25:47 -0600

    updated YouTubeAPI class to take video.title, video.description and video.publishedDate

--- GoogleYouTubeApi/YouTubeApi.cs
+++ GoogleYouTubeApi/YouTubeApi.cs
@@ -1,32 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.YouTube.v3;
+﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
 using System.IO;
 using System.Threading;
 
-namespace GoogleYouTubeApi
+namespace GoogleYouTubeApi.Models
 {
-    public class YouTubeApi
+    class YouTubeAPI
     {
-        private static YouTubeService ytService = AuthorizationCodeInstalledApp();
+        private static YouTubeService ytService = Auth();
+
+        public static string HttpRuntime { get; private set; }
 
         private static YouTubeService Auth()
         {
-            UserCredential creds;
-            using(var stream = new FileStream("youtube_client_secret.json", FileMode.Open, FileAccess.Read))
+            Google.Apis.Auth.OAuth2.UserCredential creds;
+            using (var stream = new FileStream("youtube_client_secret.json", FileMode.Open, FileAccess.Read))
             {
                 creds = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                        GoogleClientSecrets.Load(stream).Secrets,
-                        new[] { YouTubeService.Scope.YoutubeReadonly },
-                        "user",
-                        CancellationToken.None,
-                        new FileDataStore("YouTubeAPI")
-                        ).Result;
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { YouTubeService.Scope.YoutubeReadonly },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore("YouTubeAPI")
+                    ).Result;
             }
 
             var service = new YouTubeService(new BaseClientService.Initializer()
@@ -34,23 +32,23 @@ namespace GoogleYouTubeApi
                 HttpClientInitializer = creds,
                 ApplicationName = "YouTubeAPI"
             });
-
             return service;
+
         }
 
-        public static void GetVideoInfo(YouTubeService video)
+        public static void GetVideoInfo(YouTubeVideo video)
         {
             var videoRequest = ytService.Videos.List("snippet");
             videoRequest.Id = video.id;
 
             var response = videoRequest.Execute();
-            if(response.Items.Count > 0)
+
+            if (response.Items.Count > 0)
             {
                 video.title = response.Items[0].Snippet.Title;
-
+                video.description = response.Items[0].Snippet.Description;
+                video.publishedDate = response.Items[0].Snippet.PublishedAt.Value;
             }
-
         }
-
     }
 }
--- GoogleYouTubeApi/YouTubeVideo.cs
+++ GoogleYouTubeApi/YouTubeVideo.cs
@@ -1,20 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace GoogleYouTubeApi
+
+namespace GoogleYouTubeApi.Models
 {
     public class YouTubeVideo
     {
         public string id, title, description;
         public DateTime publishedDate;
-
-
         public YouTubeVideo(string id)
         {
             this.id = id;
-            YouTubeApi.GetVideoInfo(this);
+            YouTubeAPI.GetVideoInfo(this);
+
         }
+
     }
 }
